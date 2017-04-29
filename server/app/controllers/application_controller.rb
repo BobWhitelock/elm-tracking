f830c69829e7b1418b33a79c5c24e32e09b95948
Bob Whitelock <bob.whitelock1@gmail.com>
2017-04-29 13:08:32 +0100

    Setup jsonapi-resources
    Following guide at https://github.com/cerebris/peeps.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  include JSONAPI::ActsAsResourceController
+
+  # Prevent CSRF attacks.
+  protect_from_forgery with: :null_session
 end
